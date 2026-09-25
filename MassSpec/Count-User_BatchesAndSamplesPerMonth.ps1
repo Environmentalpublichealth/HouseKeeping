2d8a2456a4a5920c08bfa07f4b6b255ad7b43678
Jiali Yu <26147129+jiali0411@users.noreply.github.com>
2026-09-25 09:58:32 -0500

    Update and rename Count-BatchesAndSamplesByMonth.ps1 to Count-User_BatchesAndSamplesPerMonth.ps1
    Count a single user for all months

--- MassSpec/Count-User_BatchesAndSamplesPerMonth.ps1
+++ MassSpec/Count-User_BatchesAndSamplesPerMonth.ps1
@@ -3,7 +3,7 @@
 # Compatible with PowerShell 2.0
 #
 # Usage:
-#   .\Count-BatchesAndSamplesByMonth.ps1 "C:\Path\To\User"
+#   .\Count-User_BatchesAndSamplesPerMonth.ps1 "C:\Path\To\User"
 #   (if no path is given, it will prompt you to enter one)
 
 param(
